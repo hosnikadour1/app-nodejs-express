8563e2f8730cd904a7f4069e48281ae6e1c404b2
hosnikadour1 <kadour.hosni44@gmail.com>
2022-05-25 10:11:28 +0100

    update jenkinsfile

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 
 const express = require('express');
 
+
 // Constants
 const PORT = 3001;
 const HOST = '0.0.0.0';
 
+
+
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Devops v2');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+ 
+    res.send('Hello Devops v2');
+  });
+  
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
